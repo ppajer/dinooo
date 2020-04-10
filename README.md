8906041233baec4dcfcc2b7796e56847555890af
ppajer <peter.p.pajer@gmail.com>
2020-04-10 18:09:56 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# dinooo
+# Dinooo
+
+A bot that play Chrome's offline runner minigame for you. For when you're so bored you want to spend the day watching a bot beat its own high score while cheering for a cute pixel dinosaur.
+
+# Usage
+
+- Open [chrome://dino](chrome://dino)
+- Open console (F12)
+- Copy the contents of ˙dino.js˙ to the console & hit enter
+- Click in the main browser window and hit space to start the game
+- Be happy
+
+# Roadmap
+
+- Make into Chrome extension (obviously important)
+- Improve decision making until it can reliably break the score counter
---
+++ dino.js
@@ -0,0 +1,51 @@
+ (function Dinooo() {
+
+  var canvas = document.querySelector('.runner-canvas'),
+      ctx = canvas.getContext('2d'),
+      speed = 0,
+      maxSpeed = 200,
+      colliders = {
+        low: {y: 90, height: 35, width: 30},
+        high: {y: 75, height: 10, width: 30}
+      },
+      lock = false;
+
+  function control() {
+    var high = colliders.high,
+        low = colliders.low;
+
+    //Low
+    if(detectCollision(100, low.y, low.width+speed, low.height)) {
+      release('ArrowDown', 40);
+      key(' ', 32);
+      lock = true;
+    } else {
+      lock = false;
+    }
+
+    //High
+    if(detectCollision(100, high.y, high.width, high.height) && !lock) {
+      release(' ', 32);
+      key('ArrowDown', 40);
+    }
+  }
+
+  function increaseSpeed() {
+    if (speed < maxSpeed) speed++;
+  }
+
+  function detectCollision(x,y,w,h) {
+    return ctx.getImageData(x,y,w,h).data.indexOf(83) !== -1;
+  }
+
+  function key(key, which) {
+    document.dispatchEvent(new KeyboardEvent('keydown', {key: key, which: which, keyCode: which, code: key}));
+  }
+
+  function release(key, which) {
+    document.dispatchEvent(new KeyboardEvent('keyup', {key: key, which: which, keyCode: which, code: key}));
+  }
+
+  setInterval(control.bind(this),10);
+  setInterval(increaseSpeed.bind(this), 4000);
+})()
